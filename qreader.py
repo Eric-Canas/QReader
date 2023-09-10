4330843bc559b1304bf63701a059a1d6cc58aed5
Eric-Canas <elcorreodeharu@gmail.com>
2023-09-10 12:06:29 +0200

    Preprocessing for light QRs over Dark backgrounds

--- qreader.py
+++ qreader.py
@@ -93,7 +93,8 @@ class QReader:
         return None
 
     def decode_qr_zbar(self, image: np.ndarray,
-                       detection_result: dict[str, np.ndarray|float|tuple[float|int, float|int]]) -> list[Decoded]:
+                       detection_result: dict[str, np.ndarray | float | tuple[float | int, float | int]]) -> list[
+        Decoded]:
         """
         Try to decode the QR code just with pyzbar, pre-processing the image if it fails in different ways that
         sometimes work.
@@ -107,47 +108,48 @@ class QReader:
         decodedQR = decodeQR(image=image, symbols=[ZBarSymbol.QRCODE])
         if len(decodedQR) > 0:
             return decodedQR
-
         # Crop the QR for bbox and quad
         cropped_bbox, _ = crop_qr(image=image, detection=detection_result, crop_key=BBOX_XYXY)
         cropped_quad, updated_detection = crop_qr(image=image, detection=detection_result, crop_key=PADDED_QUAD_XY)
-        corrected_perspective = self.__correct_perspective(image=cropped_quad, padded_quad_xy=updated_detection[PADDED_QUAD_XY])
+        corrected_perspective = self.__correct_perspective(image=cropped_quad,
+                                                           padded_quad_xy=updated_detection[PADDED_QUAD_XY])
 
         for scale_factor in (1, 0.5, 2, 0.25):
-            # If rescaled_image will be larger than 1024px, skip it
-            if not all(25 < axis < 1024 for axis in corrected_perspective.shape[:2]) and scale_factor != 1:
-                continue
-            rescaled_bbox_image = cv2.resize(src=cropped_bbox, dsize=None, fx=scale_factor, fy=scale_factor,
-                                        interpolation=cv2.INTER_CUBIC)
-            decodedQR = decodeQR(image=rescaled_bbox_image, symbols=[ZBarSymbol.QRCODE])
-            if len(decodedQR) > 0:
-                return decodedQR
+            for image in (cropped_bbox, corrected_perspective):
+                # If rescaled_image will be larger than 1024px, skip it
+                if not all(25 < axis < 1024 for axis in image.shape[:2]) and scale_factor != 1:
+                    continue
 
-            rescaled_image = cv2.resize(src=corrected_perspective, dsize=None, fx=scale_factor, fy=scale_factor,
-                                        interpolation=cv2.INTER_CUBIC)
-            decodedQR = decodeQR(image=rescaled_image, symbols=[ZBarSymbol.QRCODE])
-            if len(decodedQR) > 0:
-                return decodedQR
+                rescaled_image = cv2.resize(src=image, dsize=None, fx=scale_factor, fy=scale_factor,
+                                            interpolation=cv2.INTER_CUBIC)
+                decodedQR = decodeQR(image=rescaled_image, symbols=[ZBarSymbol.QRCODE])
+                if len(decodedQR) > 0:
+                    return decodedQR
+                # For QRs with black background and white foreground, try to invert the image
+                decodedQR = decodeQR(image=255 - rescaled_image, symbols=[ZBarSymbol.QRCODE])
+                if len(decodedQR) > 0:
+                    return decodedQR
 
-            # If it not works, try to parse to grayscale (if it is not already)
-            if len(rescaled_image.shape) == 3:
-                assert rescaled_image.shape[2] == 3, f'Image must be RGB or BGR, but it has {corrected_perspective.shape[2]} channels.'
-                gray = cv2.cvtColor(rescaled_image, cv2.COLOR_RGB2GRAY)
-            else:
-                gray = rescaled_image
-            decodedQR = self.__threshold_and_blur_decodings(image=gray, blur_kernel_sizes=((5, 5), (7, 7)))
-            if len(decodedQR) > 0:
-                return decodedQR
+                # If it not works, try to parse to grayscale (if it is not already)
+                if len(rescaled_image.shape) == 3:
+                    assert rescaled_image.shape[
+                               2] == 3, f'Image must be RGB or BGR, but it has {image.shape[2]} channels.'
+                    gray = cv2.cvtColor(rescaled_image, cv2.COLOR_RGB2GRAY)
+                else:
+                    gray = rescaled_image
+                decodedQR = self.__threshold_and_blur_decodings(image=gray, blur_kernel_sizes=((5, 5), (7, 7)))
+                if len(decodedQR) > 0:
+                    return decodedQR
 
-            if len(rescaled_image.shape) == 3:
-                # If it not works, try to sharpen the image
-                sharpened_gray = cv2.cvtColor(cv2.filter2D(src=rescaled_image, ddepth=-1, kernel=_SHARPEN_KERNEL),
-                                             cv2.COLOR_RGB2GRAY)
-            else:
-                sharpened_gray = cv2.filter2D(src=rescaled_image, ddepth=-1, kernel=_SHARPEN_KERNEL)
-            decodedQR = self.__threshold_and_blur_decodings(image=sharpened_gray, blur_kernel_sizes=((3, 3),))
-            if len(decodedQR) > 0:
-                return decodedQR
+                if len(rescaled_image.shape) == 3:
+                    # If it not works, try to sharpen the image
+                    sharpened_gray = cv2.cvtColor(cv2.filter2D(src=rescaled_image, ddepth=-1, kernel=_SHARPEN_KERNEL),
+                                                  cv2.COLOR_RGB2GRAY)
+                else:
+                    sharpened_gray = cv2.filter2D(src=rescaled_image, ddepth=-1, kernel=_SHARPEN_KERNEL)
+                decodedQR = self.__threshold_and_blur_decodings(image=sharpened_gray, blur_kernel_sizes=((3, 3),))
+                if len(decodedQR) > 0:
+                    return decodedQR
 
         return []
 
@@ -244,6 +246,6 @@ class QReader:
 
 
         if return_detections:
-            return tuple(zip(decoded_qrs, detections))
+            return decoded_qrs, detections
         else:
             return decoded_qrs
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='qreader',
-    version='3.6',
+    version='3.8',
     packages=find_namespace_packages(),
     # expose qreader.py as the unique module
     py_modules=['qreader'],
